341b2814576ff36ea76162dec49022ebf99318c0
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-09-13 00:16:33 +0500

    fixed tailwind not working error

--- nextjs-commerce/tailwind.config.js
+++ nextjs-commerce/tailwind.config.js
@@ -2,7 +2,11 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+  './app/**/*.{js,ts,jsx,tsx}',
+  './src/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}'
+],
   theme: {
     extend: {
       fontFamily: {
