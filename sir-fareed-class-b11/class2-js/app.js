84e7f0d348ab7a5d349b676da5ad474226e5d85a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-02-28 12:47:19 +0500

    sir fareed class 26 feb

---
+++ sir-fareed-class-b11/class2-js/app.js
@@ -0,0 +1,78 @@
+// alert('Shehzad')
+
+// Text = sting
+
+var myName = "Shehzad Iqbal";
+
+// console.log(myName);
+
+myName = "Ahmed";
+
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+
+// module one passed students
+
+var moduleOnePassedStudents = "Shehzad";
+
+var age = 50;
+var Age = 50.5;
+
+// console.log(age + Age);
+
+var balance = 2;
+
+// console.log("idher", balance);
+
+// Recharge
+balance = balance + 10;
+
+// console.log("Mera Balance ", balance);
+
+// console.log(3 + 4 * 5);
+// console.log(7 * 5);
+// console.log(35);
+// 35, 23
+// console.log(3 + 4 * 5);
+// console.log(3 + 20);
+// console.log(23);
+
+// % is the modulus operator
+// console.log(35 % 4); // gives remainder
+
+var score = 7;
+
+// score++ // pre Increment
+
+// console.log("Score ", score--);// 5
+// console.log("Score ", score);
+
+// score++;// Post Increment
+
+var num = 1;
+
+var newNum = ++num;
+
+// BODMAS
+// console.log(1 + (3 * ( 4 + 1 ) ) / 2 * 4);
+// console.log(1 + (3 *  5  ) / 2 * 4);
+// console.log(1 + 15 / 2 * 4);
+// console.log(1 + 7.5 * 4);
+// console.log(1 + 30);
+// console.log(31);
+// 31
+
+// var message = "Hello" + "Shehzad";
+
+// console.log(message);
+
+var userName = prompt("Enter you name");
+
+alert("Hello " + userName);
