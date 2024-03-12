0c72147dd4da2d35dadffc85af6a3021731f81bb
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-12 20:09:25 +0500

    code un-commented

--- sir-noor-b11/JavaScript/loops/app.js
+++ sir-noor-b11/JavaScript/loops/app.js
@@ -1,10 +1,9 @@
-// let students = ["Shehzad","Ahmed","Fatima","Ayesha"]
-
 // for (initialization; condition; increment) {
 // 	console.log("Shehzad", i);
-//}
-
-// for (let i = 0; i < 4; i++) {
-//   console.log("Shehzad", i);
 // }
 
+for (let i = 0; i < 4; i++) {
+  console.log("Shehzad", i);
+}
+
+// let students = ["Shehzad", "Ayesha", "Ahmed", "Fatima"];
