b6c3199a0dd445227010cc12ff746ae66d7eb978
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-12 07:57:47 +0500

    sir noor first class

---
+++ sir-noor/JavaScript/array/app.js
@@ -0,0 +1,47 @@
+// let fruit1 = "Apple";
+// let fruit2 = "Banana";
+// let fruit3 = "Pineapple";
+
+let fruits = ["Apple", "Banana", "Pineapple"];
+
+// console.log(fruits[1]);
+// console.log(fruits.at(-1)); // last values return kare ga
+// console.log(fruits[fruits.length - 1]);
+// console.log(fruit2);
+
+// let stringOfArr = fruits.join("-")
+
+// fruits.push("sfdf", "test"); // add values at END
+// let returnOfPush = fruits.push("yellow"); // push returns length
+
+// fruits.pop(); // removes last value
+// fruits.pop();
+// let returnOfPop = fruits.pop(); // returns deleted values
+// fruits.pop();
+
+
+// fruits.unshift("shehzad")
+// fruits.shift()
+
+console.log(fruits.includes("Banana"));// batata hy value array ma mojood hy ya nhi 
+// console.log([2,3,4].join("="));
+
+let arr3 = new Array(10);
+let arr4 = [10];
+// console.log("🚀 ~ arr3:", arr3)
+
+let arr = ["hi there", 5, true];
+// console.log(typeof arr[4]);
+
+let arr5 = ["my string"];
+
+// arr5[0] = "new string";
+// arr5[0] = 6;
+
+const arr6 = ["my string"];
+
+arr6[0] = 78;
+
+// console.log("test", arr6);
+
+let s = "shehzad";
---
+++ sir-noor/JavaScript/loops/app.js
@@ -0,0 +1,10 @@
+// let students = ["Shehzad","Ahmed","Fatima","Ayesha"]
+
+// for (initialization; condition; increment) {
+// 	console.log("Shehzad", i);
+//}
+
+// for (let i = 0; i < 4; i++) {
+//   console.log("Shehzad", i);
+// }
+
