54393a825f2f62421ec747a0cff84d8cb8c788e0
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-15 18:15:02 +0500

    loop class

--- sir-noor-b11/JavaScript/loops/app.js
+++ sir-noor-b11/JavaScript/loops/app.js
@@ -1,9 +1,27 @@
 // for (initialization; condition; increment) {
-// 	console.log("Shehzad", i);
+// 	loop body
 // }
 
-for (let i = 0; i < 4; i++) {
-  console.log("Shehzad", i);
+let i = 1;
+
+for (
+  console.log("initialization running");
+  i <= 4;
+  console.log("increment running")
+) {
+  console.log("body i=", i);
+  i++;
 }
 
-// let students = ["Shehzad", "Ayesha", "Ahmed", "Fatima"];
+let students = ["shehzad", "Sharjeel", "Fatima", "konain"];
+
+console.log(students.length);
+
+for (let i = 0; i < students.length; i++) {
+  console.log(i);
+  console.log(students[i]);
+}
+
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
